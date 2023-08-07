67cd2e2d505eec181ac23e2f122b5b2294cc5954
Akash <akashathnure40@gmail.com>
2023-08-07 23:51:37 +0530

    added linked login auth

--- src/modules/home/components/login.js
+++ src/modules/home/components/login.js
@@ -1,9 +1,36 @@
-import React from 'react'
-import { Grid, Icon } from 'semantic-ui-react'
-import imgage  from "../../../images/loginpageImg.png"
+import React, { useState, useEffect } from 'react'
+import { Grid, Icon, Modal, Button } from 'semantic-ui-react'
+import imgage from "../../../images/loginpageImg.png"
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 import "./login.css"
 
 const Login = () => {
+
+    const [isModalOpen, setModalOpen] = useState({ open: false, msg: "" })
+
+    const clientId = '86cilhpcnozw4l';
+    const redirectUri = 'http://localhost:3000';
+    const scope = 'r_emailaddress,r_liteprofile';
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const getUrl = window.location.href
+        let code = getUrl ? getUrl.split("?")[1] ? getUrl.split("?")[1] : "" : ""
+
+        if (code && code !== "" && code !== undefined && code !== null) {
+            console.log('code', code)
+            navigate("/welcome")
+        }
+
+    }, []);
+
+    const signUpWithLinkedFunction = () => {
+        setModalOpen({ open: false, msg: "" })
+        window.location.href = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${encodeURIComponent(scope)}`;
+    }
+
     return (
         <div>
             <div className="get-started">
@@ -23,10 +50,12 @@ const Login = () => {
                             <div className="right-grid">
                                 <div className="get-started-parent">
                                     <div className="sign-up-with">Get Started</div>
-                                    <button className="linkedin-btn">
+
+                                    <button className="linkedin-btn" onClick={() => signUpWithLinkedFunction()}>
                                         <Icon name='linkedin' size='large' />
                                         <div className="sign-up-with">Sign up with Linkedin</div>
                                     </button>
+
                                 </div>
                                 <div className="login-ul-container">
                                     <ul className="login-page-ul">
@@ -48,6 +77,17 @@ const Login = () => {
 
                     </Grid.Row>
                 </Grid>
+                <Modal open={isModalOpen.open} size="mini">
+                    {/* <Modal.Header>Finance Bank List</Modal.Header> */}
+                    <Modal.Content>
+
+                    </Modal.Content>
+                    <Modal.Actions>
+
+                        <Button type="button" positive onClick={() => setModalOpen({ open: false, msg: "" })}>Close</Button>
+
+                    </Modal.Actions>
+                </Modal>
             </div>
             <footer className="getlista-2023-">{`© getlist{a} 2023 - All rights reserved.`}</footer>
         </div>
